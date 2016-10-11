f26d20a55e523be75262f65e8894b40da25dd990
Casey Bones <blank>
2016-10-11 14:49:40 +1100

    Fixed broken deps

--- src/server/server.js
+++ src/server/server.js
@@ -1,22 +1,21 @@
-import {Server} from 'hapi';
+import { Server } from 'hapi';
 import Inert from 'inert';
 import Vision from 'Vision';
 import Path from 'path';
-import Crate from 'node-crate';
 import FileSystem from 'fs';
 
 var wordDictionary = JSON.parse(FileSystem.readFileSync('./easy-words.json', 'utf8'));
 console.log(wordDictionary.length + " words loaded from file.");
 
 const server = new Server();
-server.connection( {port: 3000} );
+server.connection({ port: 3000 });
 
 const plugins = [
   { register: Inert },
   { register: Vision },
 ];
 
-server.register(plugins, function(error) {
+server.register(plugins, function (error) {
   if (error) {
     return console.error(error);
   }
@@ -34,7 +33,7 @@ server.register(plugins, function(error) {
   server.route({
     method: 'GET',
     path: '/words/random',
-    handler: function(request, response) {
+    handler: function (request, response) {
       var randomWord = wordDictionary[Math.floor(Math.random() * wordDictionary.length)];
       return response(randomWord);
     }
